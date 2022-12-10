201d00761bf1a747f3d022cd728c54c09cee24be
Dan Payne <93457949+dpayne5532@users.noreply.github.com>
2022-12-10 04:27:05 +0000

    optimize

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 // ========Bubble=Sort============================
 
 var arr = [5, 1, 24, 64, 43, 123, 11, 22, 55, 76, 34, 21, 12, 35, 121, 45];
+var arr2 = [1, 2, 3, 4, 5, 6, 7]
 
 function bubbleSort(arr) {
+  var noSwaps = true;
   for (var i = arr.length; i > 0; i--) {
     for (var j = 0; j < i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         swap(arr, j, j + 1)
-        
+        noSwaps = false;
       }
-      
+    }
+    if (noSwaps) {
+      break;
     }
   }
   console.log(arr)
