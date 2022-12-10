294134ed49ad87d81bb4177204c4895cedf18ea3
Dan Payne <93457949+dpayne5532@users.noreply.github.com>
2022-12-10 04:37:47 +0000

    add selectionSort()

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-// ========Bubble=Sort============================
+
 
 var arr = [5, 1, 24, 64, 43, 123, 11, 22, 55, 76, 34, 21, 12, 35, 121, 45];
-var arr2 = [1, 2, 3, 4, 5, 6, 7]
+var arr2 = [1, 2, 3, 4, 5, 2, 6, 7]
+
+// =======SWAP=HELPER=FUNCTION=====================
+
+function swap(arr, idx1, idx2) {
+  var temp = arr[idx1];
+  arr[idx1] = arr[idx2];
+  arr[idx2] = temp;
+}
+
+// ========Bubble=Sort============================
+
 
 function bubbleSort(arr) {
   var noSwaps = true;
@@ -17,16 +28,22 @@ function bubbleSort(arr) {
       break;
     }
   }
-  console.log(arr)
   return arr;
 }
 
-function swap(arr, idx1, idx2) {
-  var temp = arr[idx1];
-  arr[idx1] = arr[idx2];
-  arr[idx2] = temp;
+// bubbleSort(arr);
+
+// ==============================SELECTION=SORT==========================
+
+function selectionSort(arr) {
+  var min = arr[0];
+  for (var i = 1; i < arr.length; i++) {
+    if (arr[i] < min) {
+      
+    }
+  }
 }
 
-bubbleSort(arr);
 
-// ========================================================
+
+
